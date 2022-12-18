ab6a34ffe39e5e6c0af97f3a9b05fd5a7b03919d
Jebin Jolly Abaham <40578668+JebinAbraham@users.noreply.github.com>
2022-12-18 11:41:23 +0530

    Update service.py

--- EmAP/service.py
+++ EmAP/service.py
@@ -3,6 +3,6 @@ from modules import data_extraction
 
 input_dir = "data/input"
 output_dir = "data/processed_data"
-file_name = os.listdir(input_dir)[0]
+file_name = os.listdir(input_dir)
 splitval = "5"
-data_extraction.process_video(input_dir, output_dir, file_name ,splitval)
+data_extraction.process_video(input_dir, output_dir, file_name[0] ,splitval)
